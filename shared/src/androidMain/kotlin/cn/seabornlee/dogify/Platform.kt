79ee9a14e34b30b984bad86157b78bf887a9fcb5
Seaborn Lee <seabornlee.cn@gmail.com>
2022-03-08 15:49:20 +0800

    Init project

---
+++ shared/src/androidMain/kotlin/cn/seabornlee/dogify/Platform.kt
@@ -0,0 +1,5 @@
+package cn.seabornlee.dogify
+
+actual class Platform actual constructor() {
+    actual val platform: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+}
---
+++ shared/src/commonMain/kotlin/cn/seabornlee/dogify/Platform.kt
@@ -0,0 +1,5 @@
+package cn.seabornlee.dogify
+
+expect class Platform() {
+    val platform: String
+}
